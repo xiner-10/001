00fa980768a4eef6ba31b4ed7015d84fe3d762e1
xiner-10 <xiner10000@gmail.com>
2024-12-10 20:56:34 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = 'a97318c9-8e82-4263-9ed7-80c7846087e5';
+let proxyIP = '158.224.83.138';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
