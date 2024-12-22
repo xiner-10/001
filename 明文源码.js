fa69c71a42eb2c5ffba54fcfed3d7ae73cf0214b
xiner-10 <xiner10000@gmail.com>
2024-12-22 09:06:38 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '96d525d1-0b92-4fcd-bb25-a8dc76548965';
 
-const proxyIPs = [''];
+const proxyIPs = ['199.34.228.50'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
